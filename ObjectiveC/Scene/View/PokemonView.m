0f6eaf10643af49ffdf0037ba51a039b068bcfc8
Zewu Chen <zewu.chen@etec.sp.gov.br>
2024-07-11 21:23:46 -0300

    Add UITableView

--- ObjectiveC/Scene/View/PokemonView.m
+++ ObjectiveC/Scene/View/PokemonView.m
@@ -1,8 +1,10 @@
-#import <UIKit/UIKey.h>
+#import <UIKit/UIKit.h>
 #import "PokemonView.h"
 #include "PokemonProtocols.h"
 
 @interface PokemonView ()
+@property(nonatomic, strong) UITableView *tableView;
+
 @property(nonatomic) void(^didTapLoading)(void);
 @property(nonatomic) void(^didTapError)(void);
 @property(nonatomic) void(^didTapReady)(void);
@@ -10,10 +12,70 @@
 
 @implementation PokemonView
 
+- (instancetype) initWithFrame: (CGRect) frame {
+    self = [super initWithFrame: frame];
+    self.tableView = [[UITableView alloc] initWithFrame: CGRectZero
+                                                  style: UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self addSubview: self.tableView];
+    [self setupConstraints];
+    return self;
+}
+
+- (void) setupConstraints { 
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [NSLayoutConstraint activateConstraints:@[
+        [self.tableView.topAnchor constraintEqualToAnchor:self.topAnchor],
+        [self.tableView.leadingAnchor constraintEqualToAnchor:self.leadingAnchor],
+        [self.tableView.trailingAnchor constraintEqualToAnchor:self.trailingAnchor],
+        [self.tableView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor]
+    ]];
+}
+
 - (void) showLoading { }
 
 - (void) showError { }
 
 - (void) showReady { }
+
+@end
+
+#pragma mark - UITableViewDataSource
+@interface PokemonView (PokemonViewDataSourceCategory)<UITableViewDataSource>
+@end
+
+@implementation PokemonView (PokemonViewDataSourceCategory)
+
+- (NSInteger) tableView: (UITableView *) tableView numberOfRowsInSection: (NSInteger) section {
+    return 10;
+}
+
+- (UITableViewCell *) tableView: (UITableView *) tableView cellForRowAtIndexPath: (NSIndexPath *) indexPath {
+    NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: cellIdentifier];
+
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault
+                                      reuseIdentifier: cellIdentifier];
+    }
+
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %ld", (long)indexPath.row];
+
+    return cell;
+}
+
+@end
+
+#pragma mark - UITableViewDelegate
+@interface PokemonView (PokemonViewTableDelegateCategory)<UITableViewDelegate>
+@end
+
+@implementation PokemonView (PokemonViewTableDelegateCategory)
+
+- (void) tableView: (UITableView *) tableView didSelectRowAtIndexPath: (NSIndexPath *) indexPath {
+    NSLog(@"Tapped on row %ld", (long)indexPath.row);
+}
 
 @end
